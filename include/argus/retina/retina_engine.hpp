20537a27381a128b7f06230dfa27a6eb0acc568f
Hephaestus <hephaestus@local>
2026-04-03 12:59:37 +0800

    Initial commit
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ include/argus/retina/retina_engine.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace argus::retina {
+
+class RetinaEngine {
+public:
+    [[nodiscard]] static const char* name() noexcept;
+};
+
+}  // namespace argus::retina
---
+++ src/retina_engine.cpp
@@ -0,0 +1,9 @@
+#include "../include/argus/retina/retina_engine.hpp"
+
+namespace argus::retina {
+
+const char* RetinaEngine::name() noexcept {
+    return "argus-retina";
+}
+
+}  // namespace argus::retina
